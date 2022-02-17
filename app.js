08fe7d81a581ab337eda2760e2ac6f6f4eaea7c5
nwnp <jjw_0418@naver.com>
2022-02-17 21:29:45 +0900

    chore: CORS 정책 기능 구현 실패

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const nunjucks = require("nunjucks");
+const cors = require("cors");
 const app = express();
+const bodyParser = require("body-parser");
 
 const PORT = 8080;
+
+const corsOptions = {
+  origin:
+    "http://apis.data.go.kr/1471000/FoodNtrIrdntInfoService1/getFoodNtrItdntList1" /*URL*/,
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors());
+app.use(bodyParser.json());
 
 app.set("view engine", "html");
 nunjucks.configure("views", {
@@ -13,6 +24,10 @@ nunjucks.configure("views", {
 app.get("/", (req, res, next) => {
   res.render("index");
   next();
+});
+
+app.get("/products/:id", cors(corsOptions), (req, res, next) => {
+  res.json({ msg: "This is CORS-enabled for all origins!" });
 });
 
 app.listen(PORT, () => {
