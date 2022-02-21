c87a6fc4dec3b7564758fe1d87cc898c8a7be4a7
nwnp <jjw_0418@naver.com>
2022-02-21 19:21:56 +0900

    파일 삭제

--- routes/post.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const logger = require("../lib/logger");
-
-router.post("/user", (req, res, next) => {
-  logger.info(`${req.params}`);
-});
-
-module.exports = router;
