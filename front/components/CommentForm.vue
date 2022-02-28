b6af5884e0b365fe6af1df5c739778e457ae0605
nwnp <jjw_0418@naver.com>
2022-02-28 11:32:11 +0900

    fix: post

---
+++ front/components/CommentForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-form
+    ref="form"
+    v-model="valid"
+    style="position: relative"
+    @submit.prevent="onSubmitForm"
+  >
+    <v-textarea
+      v-model="content"
+      filled
+      label="댓글 달기"
+      auto-grow
+      :hide-details="hideDetails"
+      :success="success"
+      :success-messages="successMessages"
+      @input="onChangeTextArea"
+    />
+    <v-btn color="green" dark abosolute top right type="submit">응애~</v-btn>
+  </v-form>
+</template>
+
+<script>
+export default {
+  methods: {
+    onSubmitForm() {},
+    onChangeTextArea() {
+      if (value.length) {
+        this.hideDetails = true;
+        this.success = false;
+        this.successMessages = "";
+      }
+    },
+  },
+};
+</script>
