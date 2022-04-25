6f90dd9a5543f9e2c252d08b189b7ae46e3c4f97
nwnp <jjw_0418@naver.com>
2022-04-25 20:53:27 +0900

    feat: login modal

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,32 +1,58 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <b-navbar toggleable type="dark" variant="dark">
+      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+      <div style="display: flex; flexdirection: column; baseline: center">
+        <b-nav-form>
+          <b-input-group class="mb-2">
+            <b-input-group-prepend is-text>
+              <b-icon icon="search"></b-icon>
+            </b-input-group-prepend>
+            <b-form-input
+              type="search"
+              placeholder="Search terms"
+            ></b-form-input>
+          </b-input-group>
+        </b-nav-form>
+
+        <b-dropdown id="dropdown-1" text="페이지 이동" class="m-md-2">
+          <b-dropdown-item href="/">메인 페이지</b-dropdown-item>
+          <b-dropdown-item href="/about">ABOUT</b-dropdown-item>
+        </b-dropdown>
+
+        <div>
+          <b-nav-item-dropdown text="User" right>
+            <b-dropdown-item href="#">Account</b-dropdown-item>
+            <b-dropdown-item @click="onClick">Settings</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </div>
+      </div>
+    </b-navbar>
     <router-view />
+    <loginInform />
   </div>
 </template>
 
+<script>
+import loginInform from "./views/user/userInform.vue";
+
+export default {
+  components: {
+    loginInform: loginInform,
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    onClick() {
+      this.$bvModal.show("modal-login-inform");
+    },
+  },
+};
+</script>
+
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
---
+++ frontend/src/views/user/userInform.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <b-modal id="modal-login-inform" title="로그인" @ok="onSubmit">
+      <b-form-group>
+        <b-form-input
+          placeholder="이메일"
+          v-model="email"
+          required
+        ></b-form-input>
+        <b-form-input
+          placeholder="패스워드"
+          v-model="password"
+          required
+        ></b-form-input>
+      </b-form-group>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: null,
+      password: null,
+    };
+  },
+  methods: {
+    onSubmit() {
+      const payload = {
+        email: this.email,
+        password: this.password,
+      };
+      console.log(payload);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
