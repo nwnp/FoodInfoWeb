71d4e12c9ec5e83f32b623a4071b4abd83d76f06
nwnp <jjw_0418@naver.com>
2022-07-20 01:55:41 +0900

    feat: 건강기능식품 Infinite Scrolling 완료

--- frontend/src/store/food/nutritions.js
+++ frontend/src/store/food/nutritions.js
@@ -4,26 +4,50 @@ export default {
   state: {
     Nutritions: null,
     NutritionList: [],
+    page: 1,
   },
   getters: {
     Nutritions: (state) => state.Nutritions,
     NutritionList: (state) => state.NutritionList,
+    page: (state) => state.page,
   },
   mutations: {
     setNutritionList(state, payload) {
       state.NutritionList = payload;
     },
+    setNutritionInfiniteScroll(state, payload) {
+      state.NutritionList.push(...payload);
+    },
+    setPage(state, payload) {
+      state.page = payload;
+    },
   },
   actions: {
-    async actNutritionList({ commit }) {
+    async actNutritionList({ commit }, payload) {
       const url = encodeURIComponent(
-        "http://apis.data.go.kr/1471000/HtfsInfoService2/getHtfsItem?ServiceKey=iDkLT4eS%2FHMo6lNvhVqC0ls3w2lW41gVYeFPsE98tklSxWHVKd%2F5G%2F7Dbu7Xrxt4Bb%2BFsPXzcc7nNzYN16eyUA%3D%3D&numOfRows=10&type=json"
+        `http://apis.data.go.kr/1471000/HtfsInfoService2/getHtfsItem?ServiceKey=iDkLT4eS%2FHMo6lNvhVqC0ls3w2lW41gVYeFPsE98tklSxWHVKd%2F5G%2F7Dbu7Xrxt4Bb%2BFsPXzcc7nNzYN16eyUA%3D%3D&numOfRows=10&pageNo=${payload}&type=json`
       );
       await axios
         .get(`https://api.allorigins.win/raw?url=${url}`)
         .then((res) => {
           commit("setNutritionList", res.data.body.items);
-          console.log(res.data.body.items);
+        });
+    },
+    async actNutritionInfiniteScroll({ commit }, payload) {
+      const { pageNumber, state } = payload;
+      const url = encodeURIComponent(
+        `http://apis.data.go.kr/1471000/HtfsInfoService2/getHtfsItem?ServiceKey=iDkLT4eS%2FHMo6lNvhVqC0ls3w2lW41gVYeFPsE98tklSxWHVKd%2F5G%2F7Dbu7Xrxt4Bb%2BFsPXzcc7nNzYN16eyUA%3D%3D&numOfRows=10&pageNo=${pageNumber}&type=json`
+      );
+      await axios
+        .get(`https://api.allorigins.win/raw?url=${url}`)
+        .then((res) => {
+          if (res.data.body.items) {
+            commit("setNutritionInfiniteScroll", res.data.body.items);
+            commit("setPage", pageNumber);
+            state.loaded();
+          } else {
+            state.complete();
+          }
         });
     },
   },
--- frontend/src/views/nutritions/nutritions.vue
+++ frontend/src/views/nutritions/nutritions.vue
@@ -1,21 +1,27 @@
 <template>
   <div>
-    <b-list-group>
-      <b-list-group-item
-        style="margin-left: 5px; margin-right: 5px; margin-bottom: 8px"
-        v-for="(n, index) in NutritionList"
-        :key="(n, index)"
-        v-on:click="nutritionsModal(index)"
-      >
-        {{ index + 1 }}. 제품명:{{ n.PRDUCT }} 제품회사: {{ n.ENTRPS }}
-      </b-list-group-item>
-    </b-list-group>
-    <nutritionModal />
+    <div class="conatiner">
+      <main>
+        <section
+          v-for="(n, index) in NutritionList"
+          :key="(n, index)"
+          v-on:click="nutritionsModal(index)"
+        >
+          {{ index + 1 }}. 제품명:{{ n.PRDUCT }} 제품회사: {{ n.ENTRPS }}
+        </section>
+      </main>
+      <infinite-loading
+        @infinite="infiniteHandler"
+        force-use-infinite-wrapper=".el-table__body-wrapper"
+      ></infinite-loading>
+      <nutritionModal />
+    </div>
   </div>
 </template>
 
 <script>
 import NutritionsModal from "./nutritionsModal.vue";
+import InfiniteLoading from "vue-infinite-loading";
 
 export default {
   data() {
@@ -23,23 +29,62 @@ export default {
   },
   components: {
     nutritionModal: NutritionsModal,
+    InfiniteLoading,
   },
   computed: {
     NutritionList() {
       return this.$store.getters.NutritionList;
     },
+    PageNumber() {
+      return this.$store.getters.page;
+    },
   },
   created() {
-    this.searchNutritionList();
+    this.searchNutritionList(1);
   },
   methods: {
-    searchNutritionList() {
-      this.$store.dispatch("actNutritionList");
+    searchNutritionList(payload) {
+      this.$store.dispatch("actNutritionList", payload);
     },
     nutritionsModal(index) {
       console.log("index", index);
       this.$bvModal.show("modal-nutritions");
     },
+
+    /* eslint-disable no-unused-vars */
+    infiniteHandler($state) {
+      const payload = { pageNumber: this.PageNumber + 1, state: $state };
+      this.$store.dispatch("actNutritionInfiniteScroll", payload);
+    },
   },
 };
 </script>
+
+<style>
+main {
+  display: flex;
+  flex: 1;
+  flex-wrap: wrap;
+}
+
+section {
+  flex: 1 200px;
+  padding: 20px;
+  background-color: lightgray;
+  margin: 10px;
+  border: none;
+  border-radius: 7px;
+}
+
+.container {
+  display: flex;
+  justify-content: center;
+  align-content: center;
+}
+
+@media screen and (max-width: 600px) {
+  section {
+    flex: 1 100%;
+  }
+}
+</style>
